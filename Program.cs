844388126f1982ae37ba6777c6759680b8a1b0f0
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-03-11 08:24:09 +0530

    Welcome

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Linecomparision
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparision Problems");
+
+
+        }
+    }
+}
